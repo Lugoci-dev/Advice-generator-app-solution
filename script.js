c34968a23d473703e22fed6f16dbd857f98b9f55
Isaac Daniel <isaitodaniel@gmail.com>
2024-09-30 21:56:05 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const id = 117;
+
+const content = "It is easy to sit up and take notice, what's difficlut is getting un and takeing action";
+
+const button = document.addEventListener('click', search);
+
+function search(){
+    window.idText.innerText = `${id}`
+    window.idContent.innerText = `${content}`
+}
